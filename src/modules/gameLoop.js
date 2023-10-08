0f4c1943399e82fa5a52ed36f217b53f04bbbec9
Oscar Mora <oscgmora@gmail.com>
2023-10-07 21:29:33 -0400

    Begin Gameloop Implementation

---
+++ src/modules/gameLoop.js
@@ -0,0 +1,7 @@
+import Player from './factories/playerFactory';
+import Ship from './factories/shipFactory';
+
+function gameloop() {
+    const user = new Player(1);
+    const AI = new Player(2);
+}

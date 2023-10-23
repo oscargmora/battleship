99687c161f3ee5e980647c70598e701f4c0c88d5
Oscar Mora <oscgmora@gmail.com>
2023-10-22 23:09:26 -0400

    Board Styling

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-plusplus */
 function displayGameBoards(player) {
-    console.log(player);
     for (let i = 0; i < player.gameBoard.array.length; i++) {
         const gameContainer = document.querySelector(
             `#player-${player.playerNumber}`
